9ef064d32beefc6c38d1d03c4e3e1539683ed2eb
An Ki <kiyko.andriy@gmail.com>
2021-11-17 17:36:08 +0100

    showSecTeLNum

--- crm-backend/src/modules/showSecTelNum.js
+++ crm-backend/src/modules/showSecTelNum.js
@@ -1,4 +1,22 @@
 const showSecondTel = function () {
+  const headerArrow = document.querySelector('.header-contacts__arrow');
+  const secondTelMunber = document.querySelector('.header-contacts__phone-number-accord');
+  let tru = false;
+
+  headerArrow.addEventListener('click', function() {
+    tru = !tru;
+    if (tru) {
+      this.style.transform = 'rotate(180deg)';
+      secondTelMunber.style.top = '25px';
+      secondTelMunber.firstElementChild.style.opacity = 1;
+    } else {
+      console.log(this.style.transform)
+      this.style.transform = 'rotate(0deg)';
+      secondTelMunber.style.top = '0px';
+      secondTelMunber.firstElementChild.style.opacity = 0;
+    }
+
+  })
 
 }
 
